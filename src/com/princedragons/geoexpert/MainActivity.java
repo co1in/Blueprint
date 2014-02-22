23a247caa12f17805bf91b19e4d8e719dcd7b217
Colin Willoughby <colinw@savi.net>
2014-02-22 14:44:16 -0500

    Alpha Build

--- src/com/princedragons/geoexpert/MainActivity.java
+++ src/com/princedragons/geoexpert/MainActivity.java
@@ -2,15 +2,31 @@ package com.princedragons.geoexpert;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity 
+{
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button play = (Button)this.findViewById(R.id.playButton);
+		play.setOnClickListener(new OnClickListener()
+		{
+			@Override
+			public void onClick(View v)
+			{
+				Intent i = new Intent(MainActivity.this, GameActivity.class);
+				MainActivity.this.startActivity(i);
+				finish();
+			}
+		});
 	}
 
 	@Override
