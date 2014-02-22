79d5203f44c7da98309a5c5fa64b73299ca081c2
Colin Willoughby <colinw@savi.net>
2014-02-22 13:07:06 -0500

    Started game screen

--- gen/com/princedragons/geoexpert/R.java
+++ gen/com/princedragons/geoexpert/R.java
@@ -22,12 +22,21 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int indicator_off=0x7f020001;
+        public static final int indicator_on=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int ind1=0x7f080000;
+        public static final int ind2=0x7f080001;
+        public static final int ind3=0x7f080002;
+        public static final int ind4=0x7f080003;
+        public static final int ind5=0x7f080004;
+        public static final int textView1=0x7f080005;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int activity_game=0x7f030000;
+        public static final int activity_main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
---
+++ src/com/princedragons/geoexpert/GameActivity.java
@@ -0,0 +1,15 @@
+package com.princedragons.geoexpert;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		
+	}
+}
